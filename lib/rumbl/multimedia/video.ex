1a304b8567ac89bc97126165a65a65bda445252c
Roberto Ribeiro <robertov82008@gmail.com>
2021-08-25 20:14:43 -0300

    chapter 7

---
+++ lib/rumbl/multimedia/video.ex
@@ -0,0 +1,23 @@
+defmodule Rumbl.Multimedia.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "videos" do
+    field :description, :string
+    field :title, :string
+    field :url, :string
+
+    belongs_to :user, Rumbl.Accounts.User
+    belongs_to :category, Rumbl.Multimedia.Category
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:url, :title, :description, :category_id])
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
+  end
+end
---
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,31 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+
+  alias Rumbl.Accounts
+  alias RumblWeb.Auth
+
+  def new(conn, _) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => password}}) do
+    case Accounts.authenticate_by_username_and_pass(username, password) do
+      {:ok, user} ->
+        conn
+        |> Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+end
